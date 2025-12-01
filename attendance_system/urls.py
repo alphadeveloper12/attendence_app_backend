47b465ecdad170059614eae94c011c4b3c93d226
mughal41 <mughalusama43@gmail.com>
2025-12-01 22:04:59 +0500

    fix: resolved loop bug

--- attendance_system/urls.py
+++ attendance_system/urls.py
@@ -1,11 +1,18 @@
+
+from django.urls import reverse
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.shortcuts import redirect
 
+def root_redirect(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('admin-dashboard'))
+    return redirect(reverse('login'))  # direct to login instead of dashboard
+
 urlpatterns = [
-    path('', lambda request: redirect('admin-dashboard')),  # Redirect root to dashboard
+    path('', root_redirect),
     path('admin/', admin.site.urls),
     path('api/attendance/', include('attendance.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
