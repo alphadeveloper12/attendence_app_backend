fce7d6a0096d6b7c539539ede978f7a1eb45a5f1
Syed Umair <syedumairshah81@hotmail.com>
2026-04-09 16:37:05 +0400

    Update 0001_initial.py

--- hr_manpower/migrations/0001_initial.py
+++ hr_manpower/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('attendance', '0016_remove_adminprofile_site_adminprofile_sites'),
+        ('attendance', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
