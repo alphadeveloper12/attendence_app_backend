8a48dd16f2f7ba91977abbc3b42d6f8897936d60
Syed Umair <syedumairshah81@hotmail.com>
2026-05-02 17:34:58 +0400

    Update settings.py

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -103,12 +103,12 @@ WSGI_APPLICATION = 'attendance_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -119,16 +119,17 @@ DATABASES = {
 #         'PORT': '5432',
 #     }
 # }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'db_attendance2',
-#         'USER': 'dev_attendance',
-#         'PASSWORD': 'RocketOps#wnLi$',
-#         'HOST': "127.0.0.1",
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_attendance2',
+        'USER': 'dev_attendance',
+        'PASSWORD': 'RocketOps#wnLi$',
+        'HOST': "127.0.0.1",
+        'PORT': '5432',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
